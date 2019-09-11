4f746607db728579ad450414158a817dbb888737
vincenttang <10290348+vincentntang@users.noreply.github.com>
2019-09-10 22:31:37 -0400

    finish section 2 queries

--- client/src/CompanyDetail.js
+++ client/src/CompanyDetail.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { companies } from './fake-data';
+import { JobList } from './JobList';
 import {loadCompany} from './requests';
 
 export class CompanyDetail extends Component {
@@ -24,6 +25,8 @@ export class CompanyDetail extends Component {
       <div>
         <h1 className="title">{company.name}</h1>
         <div className="box">{company.description}</div>
+        <h5 className="title is-5">Jobs at {company.name}</h5>
+        <JobList jobs={company.jobs} />
       </div>
     );
   }
--- client/src/requests.js
+++ client/src/requests.js
@@ -22,6 +22,10 @@ export async function loadCompany(id) {
       id
       name
       description
+      jobs {
+        id
+        title
+      }
     }
   }`;
 
