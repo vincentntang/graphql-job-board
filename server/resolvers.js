830af462f7ad59adedef841f713ba346a1373d8d
vincenttang <10290348+vincentntang@users.noreply.github.com>
2019-09-10 22:53:03 -0400

    18 - Creating a job

--- server/resolvers.js
+++ server/resolvers.js
@@ -6,6 +6,13 @@ const Query = {
   jobs: () => db.jobs.list()
 };
 
+const Mutation = {
+  createJob: (root, {companyId, title, description}) => { 
+    console.log("MUTATION RAN")
+    return db.jobs.create({companyId, title, description}); // create is part of the fakeDB we're using
+  }
+}
+
 const Company = {
   jobs: (company) => db.jobs.list()
     .filter((job) => job.companyId === company.id)
@@ -17,6 +24,7 @@ const Job = {
 
 // Basically, check the first query
 // If it sees a nested list, match it with that specific name
-// Query, Company, Job doesn't mean anything
+// Query, Company, Job names must match the schema.graphql
+// Because it's loaded in Apolloserver
 
-module.exports = { Query, Company, Job};
+module.exports = { Query, Company, Job, Mutation}; // Make sure to specify all exports
