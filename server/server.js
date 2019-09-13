c02eedfcbbb63bcc16e6f9a361c512c7d8fdda6f
vincenttang <10290348+vincentntang@users.noreply.github.com>
2019-09-12 23:36:20 -0400

    FINISH - new version 2.0 now

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
-const {graphqlExpress, graphiqlExpress} = require('apollo-server-express');
+// const {graphqlExpress, graphiqlExpress} = require('apollo-server-express');  - VERSION 1.0 APOLLO SERVER
+const {ApolloServer, gql} = require('apollo-server-express');
 const {makeExecutableSchema} = require('graphql-tools');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -12,9 +13,8 @@ const db = require('./db');
 const port = 9002;
 const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
 
-const typeDefs = fs.readFileSync('./schema.graphql', {encoding: 'utf-8'})
+const typeDefs = gql(fs.readFileSync('./schema.graphql', {encoding: 'utf-8'})); // VERSION 2.0 pass gql function
 const resolvers = require('./resolvers');
-const schema = makeExecutableSchema({typeDefs, resolvers})
 
 const app = express();
 app.use(cors(), bodyParser.json(), expressJwt({
@@ -22,15 +22,23 @@ app.use(cors(), bodyParser.json(), expressJwt({
   credentialsRequired: false
 }));
 
-// graphql is our main endpoint
-app.use('/graphql', graphqlExpress((req) => ({
-  schema,
-  context: {user: req.user && db.users.get(req.user.sub)} //context is { id: 'BJrp-DudG', email: 'alice@facegle.io', password: 'alice123', companyId: 'HJRa-DOuG' } 
-  // context: {user: req.user} // context only has user id {id: 'BJrp-DudG'}
-})));
+const graphqlServer = new ApolloServer({
+  typeDefs, 
+  resolvers,
+  context: ({req}) => ({user: req.user && db.users.get(req.user.sub)})
+  // playground: false // for production
+}); // VERSION 2.0 - Pass a third 
+graphqlServer.applyMiddleware({app}); // VERSION 2.0 - has graphql path by default
+// graphql is our main endpoint - VERSION 1.0 APOLLO SERVER
+// const schema = makeExecutableSchema({typeDefs, resolvers})
+// app.use('/graphql', graphqlExpress((req) => ({
+//   schema,
+//   context: {user: req.user && db.users.get(req.user.sub)} //context is { id: 'BJrp-DudG', email: 'alice@facegle.io', password: 'alice123', companyId: 'HJRa-DOuG' } 
+//   // context: {user: req.user} // context only has user id {id: 'BJrp-DudG'}
+// })));
 
-// This is our playground endpoint
-app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
+// // This is our playground endpoint
+// app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
 
 // I think this is an old endpoint - tba removed later
 app.post('/login', (req, res) => {
